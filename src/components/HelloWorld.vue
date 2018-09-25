2c8dfcb733279adb96121cdfb80f817345cc985e
jan pungarsek <jan.pungarsek@gmail.com>
2018-09-25 22:37:04 +0200

    initial commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,79 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+	<div>
+		<b-form-select v-model="selected" :options="options" class="mb-3" size="sm" @change="load($event)" />
+		<div>
+			<v-runtime-template :template="template"></v-runtime-template>
+		</div>
+	</div>
 </template>
 
 <script>
+import FormComponent from '../assets/FormComponent.js';
+import ListGroupComponent from '../assets/ListGroupComponent.js';
+import VRuntimeTemplate from 'v-runtime-template';
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
+	data() {
+		return {
+			template: ``,
+			selected: null,
+			options: [
+				{ value: null, text: 'Please select an option' },
+				{ value: FormComponent, text: 'Form' },
+				{ value: ListGroupComponent, text: 'ListGroupComponent  ' },
+			],
+			form: {
+				email: '',
+				name: '',
+				food: null,
+				checked: [],
+			},
+			foods: [{ text: 'Select One', value: null }, 'Carrots', 'Beans', 'Tomatoes', 'Corn'],
+			show: true,
+		};
+	},
+	computed: {},
+	components: {
+		VRuntimeTemplate,
+	},
+	methods: {
+		load: function($event) {
+			this.template = $event;
+		},
+		onSubmit(evt) {
+			evt.preventDefault();
+			alert(JSON.stringify(this.form));
+		},
+		onReset(evt) {
+			evt.preventDefault();
+			/* Reset our form values */
+			this.form.email = '';
+			this.form.name = '';
+			this.form.food = null;
+			this.form.checked = [];
+			/* Trick to reset/clear native browser form validation state */
+			this.show = false;
+			this.$nextTick(() => {
+				this.show = true;
+			});
+		},
+	},
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 h3 {
-  margin: 40px 0 0;
+	margin: 40px 0 0;
 }
 ul {
-  list-style-type: none;
-  padding: 0;
+	list-style-type: none;
+	padding: 0;
 }
 li {
-  display: inline-block;
-  margin: 0 10px;
+	display: inline-block;
+	margin: 0 10px;
 }
 a {
-  color: #42b983;
+	color: #42b983;
 }
 </style>
