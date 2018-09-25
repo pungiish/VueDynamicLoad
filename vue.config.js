970c9608d3ca0beb51c41e22b96e9a914f528a69
jan pungarsek <jan.pungarsek@gmail.com>
2018-09-25 22:59:09 +0200

    set dev, test and prod environments.

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,24 @@
+let base = '/'
+if (process.env.NODE_ENV == 'dev') {
+	base = '/'
+	console.log('dev')
+}
+if (process.env.NODE_ENV == 'test') {
+	base = '/dist'
+	console.log('test')
+}
+if (process.env.NODE_ENV == 'prod') {
+	base = '/VueDynamicLoad'
+	console.log('prod')
+}
 module.exports = {
-    configureWebpack: {
-        resolve: {
-            alias: {
-                vue$: "vue/dist/vue.common",
-            },
-        },
-    },
+	
+	baseUrl: base,
+	configureWebpack: {
+		resolve: {
+			alias: {
+				vue$: "vue/dist/vue.common",
+			},
+		},
+	},
 };
